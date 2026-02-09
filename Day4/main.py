5c92d6c7515f51456a6590805f3d3878700279d9
EMEM <ererjomk5612@gmail.com>
2026-02-09 15:41:55 +0900

    Update main.py

--- Day4/main.py
+++ Day4/main.py
@@ -1,1 +1,22 @@
 
+from flask import Flask
+from flask_sock import Sock
+
+conn = []
+
+@sock.route("/ws")
+def websocket(ws):
+  conn.append(ws)
+  while True:
+    data = ws.receive()
+    if data is None:
+      break
+  conn.remove(ws)
+
+def background_job():
+  while True:
+    time.sleep(5)
+    for ws in list(conn):
+      ws.send("5초마다 알림 발송")
+
+threading.Thread(target=background_job, demon=True).start()
